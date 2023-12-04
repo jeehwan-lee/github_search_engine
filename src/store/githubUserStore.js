7e0c4a0e4f506cb25160af7b67e03617df14d7f9
jeehwan <tjtojan@naver.com>
2023-12-04 19:58:49 +0900

    userProfile store 생성

---
+++ src/store/githubUserStore.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import create from "zustand";
+
+export const useGithubUserStore = create((set) => ({
+  user: {},
+  loading: false,
+  getUser: async (username) => {
+    set({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users/${username}`
+    );
+
+    set({ loading: false, user: res.data });
+  },
+}));
